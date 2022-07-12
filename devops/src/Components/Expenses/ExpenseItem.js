5208228a7f6075e7f47b473d1f6d1ee4e0f759b7
anilbhangay <anilbhangay@gmail.com>
2022-07-12 14:54:28 +0530

    adding new form

--- devops/src/Components/Expenses/ExpenseItem.js
+++ devops/src/Components/Expenses/ExpenseItem.js
@@ -8,25 +8,17 @@ import Card from '../UI/Card';
 
 const ExpenseItem = (props) => {
 
-  const [newTitle, setNewTitle] = useState("");
-    
-   const [Title, setTitle] = useState(props.Title);
 
-    const clickHandler = () =>{
-         setTitle('New Title..');
-    }
-        
 
      return (
 
         <div className="expense-item">
         <ExpenseDate Date={props.Date} />
         <div className="expense-item__description">
-            <h2>{ Title }</h2>
+            <h2>{ props.Title }</h2>
              <div className="expense-item__price">${props.Amount}</div>
       </div>
-          <input type= "text" value= "newTitle" />
-            <button onClick={clickHandler}>Change Title</button>
+         
         </div>   
      
   );
