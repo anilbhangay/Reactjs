41cf1c7b9eff22e10a7ab049a22f95a169987a59
anilbhangay <anilbhangay@gmail.com>
2022-07-12 21:38:08 +0530

    added new expense

--- devops/src/Components/NewExpense/ExpenseForm.js
+++ devops/src/Components/NewExpense/ExpenseForm.js
@@ -18,21 +18,35 @@ const ExpenseForm = () => {
   const dateChangeHandler = (event) => {
     setEnteredDate(event.target.value);
   };   
+     const submitHandler = (event) => {
+            event.preventDefault();
 
+            const expenseData = {
+                title : enteredTitle,
+                amount : enteredAmount,
+                date : new Date(enteredDate)
+            }
+           console.log(expenseData);
+     
+            setEnteredTitle('');
+            setEnteredAmount('');
+            setEnteredDate('');
+        };
+        
     return(
-              <form>
+              <form onSubmit={submitHandler}>
                  <div className="new-expense__controls">
                  <div className="new-expense__control">
                     <label>Title</label>
-                   <input type="text" onChange={titleChangeHandler} />
+                   <input type="text" value={enteredTitle} onChange={titleChangeHandler} />
                  </div>
                  <div className="new-expense__control">
                     <label>Amount</label>
-                   <input type="number" min="0.01" step="0.01" onChange={amountChangeHandler} />
+                   <input type="number" value={enteredAmount} min="0.01" step="0.01" onChange={amountChangeHandler} />
                  </div>
                  <div className="new-expense__control">
                     <label>Date</label>
-                   <input type="date"  onChange={dateChangeHandler}/>
+                   <input type="date" value={enteredDate} onChange={dateChangeHandler}/>
                  </div>
                  </div>
                  <div className="new-expense__actions">
